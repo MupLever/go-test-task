657f0cfd0efb6f9a0294423e12a2f213b411364a
shlyzhas <8willox8@gmail.com>
2023-05-01 19:52:17 +0300

    start of working with database

---
+++ internal/app/store/store.go
@@ -0,0 +1,18 @@
+package store
+
+type Store struct {
+	config *Config
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		config: config,
+	}
+}
+
+func (s *Store) Open() error {
+	return nil
+}
+
+func (s *Store) Close() {
+}
